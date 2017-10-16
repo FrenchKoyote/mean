78bd844d30888f933b66564411e597c321f6cbd8
GALMICHE <benoit.galmiche@capgemini.com>
2017-10-16 10:33:29 +0200

    api.js modification

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-//const MongoClient = require('mongodb').MongoClient;
-//const ObjectID = require('mongodb').ObjectID;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
 // Connect
 const connection = (closure) => {
     return MongoClient.connect('mongodb://localhost:27017/mean', (err, db) => {
-        if (err) return console.log(err);
-
+        if (err) throw err;
         closure(db);
     });
 };
@@ -29,21 +28,27 @@ let response = {
 // Get users
 router.get('/users', (req, res) => {
 
-    response.data = ['User1', 'User2', 'User3', 'User4'];
-    res.json(response);
+    //response.data = ['User1', 'User2', 'User3', 'User4'];
+    //res.json(response);
 
-    /*    connection((db) => {
-        db.collection('users')
-            .find()
-            .toArray()
-            .then((users) => {
-                response.data = users;
-                res.json(response);
-            })
-            .catch((err) => {
-                sendError(err, res);
-            });
-    });*/
+    connection((db) => {
+    db.createCollection("users", function(err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+        db.close();
+        });
+
+    db.collection('users')
+        .find()
+        .toArray()
+        .then((users) => {
+            response.data = users;
+            res.json(response);
+        })
+        .catch((err) => {
+            sendError(err, res);
+        });
+    });
 });
 
 module.exports = router;
